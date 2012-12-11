c0731fc2d5e40727865d2509f26644187ba0e8b4
Stephen Hill <stephen@gatekiller.co.uk>
2012-12-11 20:11:01 +0400

    Reworking of the autoloader.

--- src/Four/Core/Autoload.php
+++ src/Four/Core/Autoload.php
@@ -4,19 +4,29 @@ namespace Four\Core
 {
 	class Autoload
 	{
+		// Static Fields
+		public static $Directory = "";
+		
 		// Static Methods
 		public static function Autoload($class)
 		{
-			var_dump($class);
-			if (__NAMESPACE__ === "Four")
+			// Replace underscores and namespace seperators with directory seperators
+			$file = str_replace("_", DIRECTORY_SEPARATOR, $class);
+			$file = str_replace("\\", DIRECTORY_SEPARATOR, $file);
+			$file = self::$Directory . DIRECTORY_SEPARATOR . $file . ".php";
+			
+			if (file_exists($file) === true)
 			{
-				$class = substr($class, 5);
-				require_once($class . ".php");
+				require_once($file);
 			}
 		}
 
 		public static function Register()
 		{
+			// Set the autoload search directory
+			self::$Directory = realpath(dirname(__FILE__) . '/../..');
+			
+			// Register the autoloader
 			spl_autoload_register
 			(
 				__NAMESPACE__ . "\\Autoload::Autoload",
--- src/Four/Core/Kernel.php
+++ src/Four/Core/Kernel.php
@@ -5,7 +5,9 @@ namespace Four\Core
 	class Kernel
 	{
 		// Constants
-		const VERSION = 1.0;
+		const VERSION = "1.0";
+		const MAJOR_VERSION = 1;
+		const MINOR_VERSION = 0;
 		
 		// Public Fields
 		public static $Routes = array();
