3943ca66a2b1a2904f7068f2ddcc4b52cbd28285
Stephen Hill <stephen@gatekiller.co.uk>
2012-12-14 20:53:10 +0400

    Merged Kernel into Application class.

--- lib/Four/Framework/Application.php
+++ lib/Four/Framework/Application.php
@@ -6,12 +6,31 @@ namespace Four\Framework
 	
 	class Application
 	{
+		const VERSION = "1.0";
+		const MAJOR_VERSION = 1;
+		const MINOR_VERSION = 0;
+		
 		public static $Routes = new RouteCollection();
 		public static $Request;
 		
-		public static __constuct()
+		public static Init()
 		{
+			// Register the autoloader
+			require_once(__DIR__ . "/Autoload.php");
+			Autoload::Register();
 			self::$Request = new HttpRequest($_GET, $_POST, $_FILES, $_SERVER, $_COOKIES);
+			
+			// Add the default route
+			// $ExampleRoute = new Route(array(
+			// 	"Pattern" => "/{Controller}/{Action}/{Id}",
+			// 	"Controller" => "PageController",
+			// 	"Namespace" => "/",
+			// 	"Defaults" => array(
+			// 		"Controller" => "Home",
+			// 		"Action" => "Index",
+			// 		"Id" => null
+			// 	)
+			// ));
 		}
 	}
 }
