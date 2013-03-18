5de6781594e98554d55466d12b2d5a0f091d972c
Stephen Hill <stephen@gatekiller.co.uk>
2013-03-18 21:08:19 +0400

    Added encryption to the HttpCookie.

--- vendor/Four/Web/HttpCookie.php
+++ vendor/Four/Web/HttpCookie.php
@@ -6,5 +6,17 @@ namespace Four\Web
 	{
 		public $Name;
 		public $Value;
+		
+		private $_name;
+		private $_value;
+		
+		public function __construct($name, $value)
+		{
+			$this->_name = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, "sdlkfjsldkfjlksdjflksdflksdflkj", $name, MCRYPT_MODE_CBC, "asdasdasdasdasd");
+			$this->_value = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, "sdlkfjsldkfjlksdjflksdflksdflkj", $value, MCRYPT_MODE_CBC, "asdasdasdasdasd");
+			
+			$this->Name = $name;
+			$this->Value = $value;
+		}
 	}
 }
