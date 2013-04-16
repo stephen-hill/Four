769868aea1fbb7ac1b898ed94340d6c39ef00ec4
Stephen Hill <stephen@gatekiller.co.uk>
2013-04-16 19:59:13 +0400

    Trying to flesh out how routes will be defined.

---
+++ tests/Four/Framework/RouteTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Four\Framework
+{
+	# /
+	$home = new Route("/", array(
+		"Controller" => "DefaultController",
+		"Action" => "Default"
+	));
+	
+	# /user/view/stephen-hill
+	$viewUser = new Route("/user/view/{Username}");
+	
+	# /user/stephen-hill
+	$user = new Route('/User/{Username}', array(
+		"Action" => "View"
+	));
+	
+	# /blog/stephen-hill/post/123
+	$blogPost = new Route("/{Controller}/{BlogName}/{Action}/{PostID}");
+}
